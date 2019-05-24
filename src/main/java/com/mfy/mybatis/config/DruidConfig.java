f3c92674111b9231b63cf73146837bb117e80e10
mfy <2290817035@qq.com>
2019-05-24 20:53:47 +0800

    druid

--- src/main/java/com/mfy/mybatis/config/DruidConfig.java
+++ src/main/java/com/mfy/mybatis/config/DruidConfig.java
@@ -35,6 +35,7 @@ public class DruidConfig {
         //是否能够重置数据 禁用HTML页面上的“Reset All”功能
         servletRegistrationBean.addInitParameter("resetEnable", "false");
 		system.ou.println("----");
+		system.ou.println("----");
         return servletRegistrationBean;
     }
 
